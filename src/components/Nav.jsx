0d7d8b608991aa2ba3a1208649ef67dfd6b4c4b0
NaofalP <naop18@yahoo.fr>
2025-04-08 10:49:52 +0200

    header v1

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import logo from "../assets/logos/excellence-logo-png-rogne.png";
 
 export default function Header() {
     return <>
-        <nav>
-            <Link to="/">
+        <nav className="nav-header">
+            <Link to="/" className="nav-header_link">
                 Acceuil
             </Link>
-            <Link to="/">
-                LOGO
+            <Link to="/" className="nav-header_link">
+                <img src={logo} alt="logo du service de taxi excellence transfert "/>
             </Link>
-            <Link to="/Services">
+            <Link to="/Services" className="nav-header_link">
                 Services
             </Link>
         </nav>
