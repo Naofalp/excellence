efe0bce01177e1d399df101caa1003142d753787
NaofalP <naop18@yahoo.fr>
2025-04-08 13:44:24 +0200

    navbar navlink isActive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/logos/excellence-logo-png-rogne.png";
 
 export default function Header() {
     return <>
         <nav className="nav-header">
-            <Link to="/" className="nav-header_link">
+            <NavLink to="/" className={({ isActive }) => (isActive ? "nav-header_link active-link" : "nav-header_link")}>
                 Acceuil
-            </Link>
-            <Link to="/" className="nav-header_link">
-                <img src={logo} alt="logo du service de taxi excellence transfert "/>
-            </Link>
-            <Link to="/Services" className="nav-header_link">
+            </NavLink>
+            <NavLink to="/" className="nav-header_link">
+                <img
+                    src={logo}
+                    alt="logo du service de taxi excellence transfert "
+                />
+            </NavLink>
+            <NavLink to="/Services" className={({ isActive }) => (isActive ? "nav-header_link active-link" : "nav-header_link")}>
                 Services
-            </Link>
+            </NavLink>
         </nav>
     </>
 }
