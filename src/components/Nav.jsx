6170af5b57d9c332eca0d7192a6e1cff34b01004
NaofalP <naop18@yahoo.fr>
2025-04-09 12:59:59 +0200

    menu deroulant responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,27 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/logos/excellence-logo-png-rogne.png";
 import { useState } from "react";
+import { Sling as Hamburger } from "hamburger-react";
+
 
 export default function Nav() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    {/** Pour fermer le menu au click d'un lien */}
+    const handleLinkClick = () => {
+        setMenuOpen(false)
+    }
+
     return <>
-        <nav className="nav-header">
-            <NavLink to="/" className={({ isActive }) => (isActive ? "nav-header_link active-link" : "nav-header_link")}>
+
+        {/** Boutton menu depliant mobile */}
+        <button className="nav-menu-toggle">
+            <Hamburger toggled={menuOpen} toggle={setMenuOpen} size={24} color="white" label="Montrer menu" />
+        </button>
+
+        <nav className={`nav-header${menuOpen ? "_open" : ""}`}>
+            <NavLink to="/" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "nav-header_link active-link" : "nav-header_link")}>
                 Acceuil
             </NavLink>
             <NavLink to="/" className="nav-header_link">
@@ -14,7 +30,7 @@ export default function Nav() {
                     alt="logo du service de taxi excellence transfert "
                 />
             </NavLink>
-            <NavLink to="/Services" className={({ isActive }) => (isActive ? "nav-header_link active-link" : "nav-header_link")}>
+            <NavLink to="/Services" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "nav-header_link active-link" : "nav-header_link")}>
                 Services
             </NavLink>
         </nav>
