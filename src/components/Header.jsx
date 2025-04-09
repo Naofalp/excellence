d7ad4f63385faf7f77e6fb66b49c8397370c8daf
NaofalP <naop18@yahoo.fr>
2025-04-09 12:59:01 +0200

    ajout du logo et div content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import Nav from './Nav';
+import logo from "../assets/logos/excellence-logo-png-rogne.png";
+import { NavLink } from "react-router-dom";
 
 
 export default function Header() {
     return <>
         <header className='headerBand'>
-            <Nav />
+            <div className='headerBand_content'>
+                <NavLink to="/">
+                    <img
+                        src={logo}
+                        alt="logo du service de taxi excellence transfert "
+                        className="headerBand_logo"
+                    />
+                </NavLink>
+                <Nav />
+            </div>
         </header>
     </>
 }
