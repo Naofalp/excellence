f48241dd18535303530b5a577da3bb23d05da819
NaofalP <naop18@yahoo.fr>
2025-04-10 13:26:47 +0200

    test

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+<footer>
+  <p>Excellence Transfert - Chauffeur privé VTC à Montpellier, Béziers, Agde et toute l'Occitanie. Transferts en taxi, mise à disposition, et services pour événements.</p>
+  <nav>
+    <ul>
+      <li><a href="/contact">Contactez-nous</a></li>
+      <li><a href="/a-propos">À propos de nous</a></li>
+      <li><a href="/services">Nos services</a></li>
+      <li><a href="/mentions-legales">Mentions légales</a></li>
+    </ul>
+  </nav>
+</footer>
