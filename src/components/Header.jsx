614c42f98eab9d8d73fb4a72a80c4687709efb5f
NaofalP <naop18@yahoo.fr>
2025-04-10 17:08:02 +0200

    header service page black

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import Nav from './Nav';
 import logo from "../assets/logos/excellence-logo-png-rogne.png";
-import { NavLink } from "react-router-dom";
-
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
+
+    const location = useLocation();
+    const isServicesPage = location.pathname === "/Services";  
+
     return <>
-        <header className='headerBand'>
+        <header className={`headerBand ${isServicesPage ? "headerBand--red" : ""}`}>
             <div className='headerBand_content'>
                 <NavLink to="/">
                     <img
